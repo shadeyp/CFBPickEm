5c7b919561060aa716dc1c828fa82fb31f725e94
shadeyp <shadeyp@SHADEY>
2017-01-02 09:14:19 -0600

    Spinner added.

--- CFBPickEm/TeamDetailActivity.cs
+++ CFBPickEm/TeamDetailActivity.cs
@@ -5,6 +5,9 @@ using Android.Widget;
 using CFBPickEm.Core.Model;
 using CFBPickEm.Core.Service;
 using CFBPickEm.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CFBPickEm
 {
@@ -14,9 +17,15 @@ namespace CFBPickEm
         private ImageView teamDetailImageView;
         private TextView teamName;
         private TextView teamNickName;
+        private Spinner conferenceSpinner;
+        private Spinner teamSpinner;
 
         private Team selectedTeam;
+        private List<Team> teams;
+        private List<Conference> conferences;
+        private Conference selectedConference;
         private CFBDataService cfbDataService;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,10 +33,12 @@ namespace CFBPickEm
             SetContentView(Resource.Layout.TeamDetailView);
 
             CFBDataService cfbDataService = new CFBDataService();
-            selectedTeam = cfbDataService.GetTeamById(1);
+            selectedTeam = cfbDataService.GetTeamById(1);         
+            conferences = cfbDataService.GetConferences();
 
             FindViews();
             BindData();
+            SetEvents();
         }
 
         private void BindData()
@@ -37,6 +48,30 @@ namespace CFBPickEm
 
             var imageBitmap = ImageHelper.GetImageBitmapFromUrl("http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/158.png&w=100&h=100&transparent=true");
             teamDetailImageView.SetImageBitmap(imageBitmap);
+
+            //var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerItem, 
+            //    conferences.Select(c => c.ConferenceName).ToList());
+            var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerItem,
+                conferences);
+            conferenceSpinner.Adapter = adapter;
+        }
+
+        private void SetEvents()
+        {
+            conferenceSpinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(ConferenceSpinner_ItemSelected);
+        }
+
+        private void ConferenceSpinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+
+            var confId = spinner.GetItemAtPosition(e.Position).Cast<Conference>();
+            CFBDataService cfbDataService = new CFBDataService();
+            teams = cfbDataService.GetTeamsForConference(confId.ConferenceId);
+
+            var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerItem,
+                teams);
+            teamSpinner.Adapter = adapter;
         }
 
         private void FindViews()
@@ -44,6 +79,15 @@ namespace CFBPickEm
             teamDetailImageView = FindViewById<ImageView>(Resource.Id.teamDetailImageView);
             teamNickName = FindViewById<TextView>(Resource.Id.TeamNickNameTextView);
             teamName = FindViewById<TextView>(Resource.Id.TeamNameTextView);
+            conferenceSpinner = FindViewById<Spinner>(Resource.Id.conferenceSpinner);
+            teamSpinner = FindViewById<Spinner>(Resource.Id.teamSpinner);
+        }
+    }
+
+    public static class ObjectTypeHelper {
+        public static T Cast<T>(this Java.Lang.Object obj) where T : class {
+            var propertyInfo = obj.GetType().GetProperty("Instance");
+            return propertyInfo == null ? null : propertyInfo.GetValue(obj, null) as T;
         }
     }
 }
